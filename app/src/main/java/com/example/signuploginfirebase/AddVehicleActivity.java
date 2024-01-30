e8bb78173586bab156558819fa44cdc070cc5dd2
athanyuen <athany2025@student.cis.edu.hk>
2024-01-30 23:34:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/signuploginfirebase/AddVehicleActivity.java
@@ -0,0 +1,12 @@
+package com.example.signuploginfirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddVehicleActivity extends AppCompatActivity {
+    protected void oncreate(Bundle savedInstance){
+
+    }
+
+}
